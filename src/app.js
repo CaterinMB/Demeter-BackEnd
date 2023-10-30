1619158776e6df017bce2c20eff56f3294202a54
CaterinMB <cmartinez2131@misena.edu.co>
2023-10-30 00:26:49 -0500

    Controller&Routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import category_suppliesRoutes from './routes/suppliescategory.routes.js';
+import category_productsRoutes from './routes/productcategory.routes.js'
+import suppliesRoutes from '../src/routes/supplies.routes.js';
 
 const app = express();
 
+app.use(cors({
+    credentials :  true, 
+    origin: 'http://localhost:5173'
+}));
+
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(category_suppliesRoutes);
+app.use(category_productsRoutes);
+app.use(suppliesRoutes);
 
 export default app;
