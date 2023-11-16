9ef30c8986dd0fa32f2686bc3f0596fec40726d6
CaterinMB <127351464+CaterinMB@users.noreply.github.com>
2023-11-16 15:56:44 -0500

    recetas back

--- src/models/supplies.model.js
+++ src/models/supplies.model.js
@@ -3,23 +3,23 @@ import { sequelize } from "../db/dataBase.js";
 import { productDetail } from './productdetail.model.js'
 import { shoppingDetail } from './shoppingdetail.model.js'
 
-export const supplies =  sequelize.define('Supplies', {
+export const supplies = sequelize.define('Supplies', {
 
     ID_Supplies: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true 
-    }, 
+        autoIncrement: true
+    },
 
     Name_Supplies: {
-        type: DataTypes.STRING(30), 
-        allowNull: false, 
-        validate:{
-            notNull:{
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        validate: {
+            notNull: {
                 msg: "El nombre del insumo es requerido"
-            }, 
+            },
             customValidate(value) {
-                
+
                 if (!/^[A-Z][a-zA-Z\s]*$/.test(value)) {
                     throw new Error('El nombre del insumo debe comenzar con mayúscula y puede contener letras y espacios.');
                 }
@@ -33,12 +33,12 @@ export const supplies =  sequelize.define('Supplies', {
 
     Unit: {
         type: DataTypes.DOUBLE,
-        allowNull: false, 
+        allowNull: false,
         validate: {
-            notNull:{
+            notNull: {
                 msg: "La cantidad del insumo es requerido"
-            }, 
-            isInt: true, 
+            },
+            isInt: true,
             min: 0,
             max: 9999
         },
@@ -46,11 +46,11 @@ export const supplies =  sequelize.define('Supplies', {
 
     Measure: {
         type: DataTypes.STRING(15),
-        allowNull: false, 
+        allowNull: false,
         validate: {
-            notNull:{
+            notNull: {
                 msg: "La medida del insumo es requerido"
-            }, 
+            },
             customValidate(value) {
                 if (!/^[A-Za-z\s()]+$/.test(value)) {
                     throw new Error('La medida del insumo puede contener letras, espacios y paréntesis.');
@@ -63,9 +63,9 @@ export const supplies =  sequelize.define('Supplies', {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
-            notNull:{
+            notNull: {
                 msg: "El stock del insumo es requerido"
-            }, 
+            },
             isInt: true,
             min: 0,
             max: 9999
