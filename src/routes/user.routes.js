e20e55dfe0a85ac2e325120a5457ea126273e08c
CaterinMB <cmartinez2131@misena.edu.co>
2023-12-04 06:30:16 -0500

    product

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { getUsers, getUser, checkForDuplicates, createUser, updateUser, toggleUserStatus, deleteUser } from '../controllers/user.controller.js';
-import { getWaiters, createWaiter, duplicateWaiter, getWaiter } from '../controllers/user.controller.js';
+import { getUsers, getUser, checkForDuplicates, createUser, updateUser, toggleUserStatus, deleteUser } from '../controllers/user.controller.js'; // Empleados
+import { getWaiters, createWaiter, duplicateWaiter, getWaiter } from '../controllers/user.controller.js'; // Meseros
+import { editProfile, changePassword } from "../controllers/user.controller.js"; // Usuario logueado
 import { authRequired } from '../middlewares/validateToken.js'
 
 const router = Router();
@@ -11,6 +12,10 @@ router.post('/add_user', checkForDuplicates, createUser);
 router.put('/user/:id', updateUser);
 router.put("/user/toggle/:id", toggleUserStatus);
 router.delete('/user/:id', deleteUser);
+
+// --------------------------- EditProfile ------------------------------------- //
+router.put('/edit_profile/:id', editProfile);
+router.put('/change_password/:id', changePassword);
 
 // --------------------------- Mesero ------------------------------------- //
 router.get('/waiter', getWaiters);
